e592de69dbd1e10c3d8b816c3c4d54911eece415
karanagrawal369 <karanagrawal2002@gmail.com>
2024-12-16 00:49:01 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
 from textSummarizer.pipeline.Stage_01_ingestion import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.Stage_02_Validation import DataValidationTrainingPipeline
 from textSummarizer.logging import logger
 
 # Define the stage name
@@ -12,16 +13,21 @@ STAGE_NAME = "Data Ingestion stage"
 
 if __name__ == "__main__":
     try:
-        # Log the start of the stage
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
         
-        # Create and run the data ingestion pipeline
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
         data_ingestion = DataIngestionTrainingPipeline()
         data_ingestion.main()
-        
-        # Log the completion of the stage
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
-        # Log and raise any exceptions
         logger.exception(f"An error occurred during {STAGE_NAME}: {e}")
         raise
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
